e42b8c7cb0b4286d106cd8b2f546b3bf1296ae07
Franz Bernhardt <Franzbernhardt@gmail.com>
2021-05-11 15:37:48 +0200

    GitHub check fpr Understanding (commit&Push)

---
+++ src/com/ironhack/day02/forGitHub.java
@@ -0,0 +1,8 @@
+package com.ironhack.day02;
+
+public class forGitHub {
+    public static void main(String[] args) {
+        System.out.println("gitignore");
+        System.out.println("don´t gitignore");
+    }
+}
